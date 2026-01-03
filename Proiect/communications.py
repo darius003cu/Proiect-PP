e2e128d8d1caf2d709eb09c8b6e5fc7441aa385e
Darius <darius.cotiata@student.tuiasi.ro>
2026-01-03 19:58:30 +0200

    Adaugat matplotlib si serial ca module

---
+++ Proiect/communications.py
@@ -0,0 +1,8 @@
+import serial
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plotTest():
+    fig, ax = plt.subplots()
+    ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+    plt.show()
--- Proiect/windowSetup.py
+++ Proiect/windowSetup.py
@@ -1,11 +1,18 @@
 from tkinter import *
+import communications
 
 def mainWindow():
     window = Tk()
-    mainTitle = Text(window, padx = 5, pady = 5)
-    mainTitle.insert(INSERT, "Voltage")
-    mainTitle.pack()
     window.geometry("640x320")
     window.title("Voltage Graph v0.0.1")
+    button = Button(window, text = "Open debug window", command = debugWindow)
+    button.pack()
     window.mainloop()
 
+def debugWindow():
+    debugWin = Toplevel()
+    debugWin.geometry("400x300")
+    debugWin.title("Debugging for plots")
+    button1 = Button(debugWin, text = "Open plot", command = communications.plotTest)
+    button1.pack(side = "top", anchor = "center", pady = 120)
+    debugWin.mainloop()
