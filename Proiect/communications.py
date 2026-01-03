75786fbd886658bc82b01501b9151848f78e56aa
Darius <darius.cotiata@student.tuiasi.ro>
2026-01-03 20:13:29 +0200

    Adaugat plot ul principal (inca ft bare bones)

--- Proiect/communications.py
+++ Proiect/communications.py
@@ -6,3 +6,10 @@ def plotTest():
     fig, ax = plt.subplots()
     ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
     plt.show()
+
+def plotUSART():
+    fig, ax = plt.subplots()
+    ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+    plt.xlabel("Timp [s]")
+    plt.ylabel("Tensiune [V]")
+    plt.show()
